e603f24edc5975d654cc6054e834582613209098
ZucchiniZe <me@alexb.ninja>
2014-12-07 20:10:33 -0800

    maybe works

--- app.rb
+++ app.rb
@@ -1,11 +1,28 @@
 require 'sinatra'
 
-# helpers do
-#   def serve(abbr, filename, type)
-#     headers "Content-Type" => type.to_s
-#     File.read(File.join('libs', abbr.to_s, filename.to_s))
-#   end
-# end
+helpers do
+  def serve(abbr, filename)
+    case filename.split('.').last
+    when "js"
+      mime = "application/ecmascript"
+    when "css"
+      mime = "text/css"
+    when "svg"
+      mime = "image/svg+xml"
+    when "eot"
+      mime = "application/vnd.ms-fontobject"
+    when "ttf"
+      mime = "application/octet-stream"
+    when "woff"
+      mime = "application/x-woff"
+    when "gif"
+      mime = "image/gif"
+    end
+
+    headers "Content-Type" => mime
+    File.read(File.join('libs', abbr.to_s, filename.to_s))
+  end
+end
 
 before do
   headers \
@@ -18,6 +35,19 @@ get '/' do
   "<p><a href=\"http://smtcs.rocks/blog/Libraries\">List</a> of the different libraries.</p>"
 end
 
-# get '/fa/fa.css' do
-#   serve('fa', 'fa.css', 'text/css')
-# end
+get '/jq/1.js' do
+  serve('jq', 'jquery-1.js')
+end
+
+get '/jq/2.js' do
+  serve('jq', 'jquery-2.js')
+end
+
+get '/fa/fa.css' do
+  serve('fa', 'fa.css')
+end
+
+get '/fa/fonts/fontawesome-webfont.:ext' do
+  ext = params[:ext]
+  serve('fa/fonts', "fontawesome-webfont.#{ext}")
+end
