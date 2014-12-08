af8f94f9b82f2de997478060f39ef18dd84044ae
ZucchiniZe <me@alexb.ninja>
2014-12-07 21:31:26 -0800

    verify for loader

--- app.rb
+++ app.rb
@@ -77,3 +77,7 @@ end
 get '/bs/fonts/glyphicons-halflings-regular.:ext' do
   serve 'bs/fonts', "glyphicons-halflings-regular.#{params[:ext]}"
 end
+
+get '/loaderio-f5bccfe4d01d364d939024c19dbee35d' do
+  'loaderio-f5bccfe4d01d364d939024c19dbee35d'
+end
