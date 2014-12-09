ea927cb43a585be9ab59a22ba7bd33762c75f11a
ZucchiniZe <me@alexb.ninja>
2014-12-08 16:05:13 -0800

    Add analytics for server routes

--- Gemfile
+++ Gemfile
@@ -4,6 +4,8 @@ ruby '2.0.0'
 
 gem 'sinatra'
 gem 'pony'
+gem 'rest_client'
+gem 'analytics-ruby', '~> 2.0.0', :require => 'segment/analytics'
 
 group :produciton do
   gem 'passenger', '~> 5.0.0.beta1'
--- app.rb
+++ app.rb
@@ -1,6 +1,13 @@
 require 'sinatra'
 require 'json'
 require 'pony'
+require 'rest_client'
+require 'segment/analytics'
+
+Analytics = Segment::Analytics.new({
+  write_key: 'VOkH1POOQZ2csVKeZ2fLKjETIRFHD0Eo',
+  on_error: Proc.new { |status, msg| print msg }
+})
 
 set :static_cache_control, [:public, max_age: 60 * 60 * 24 * 7]
 
@@ -35,44 +42,80 @@ before do
 end
 
 get '/' do
+  Analytics.track(user_id: request.ip, event: 'Viewed Homepage')
   erb :index
 end
 
 post '/req' do
   puts params.to_json
-  Pony.mail :to => "me@alexb.ninja",
-            # :cc => "mhesby@summitps.org",
-            :from => "cdn@smtcs.rocks",
-            :subject => "#{params[:name]} Requested the #{params[:framework]}.",
-            :body => erb(:email)
+  # Pony.mail :to => "me@alexb.ninja",
+  #           # :cc => "mhesby@summitps.org",
+  #           :from => "cdn@smtcs.rocks",
+  #           :subject => "#{params[:name]} Requested the #{params[:framework]} framework.",
+  #           :body => erb(:email)
+  RestClient.post 'http://summit-hubot.herokuapp.com/hubot/newfw', params.to_json, :content_type => :json, :accept => :json
   erb :email_after
 end
 
 get '/jq/:num.js' do
+  Analytics.track(
+    user_id: request.ip,
+    event: "Hit jQuery v#{params[:num]}",
+    properties: {
+      url: request.path_info,
+      verison: params[:num]
+  })
   serve 'jq', "jquery-#{params[:num]}.js"
 end
 
 get '/fa/fa.css' do
+  Analytics.track(
+    user_id: request.ip,
+    event: "Hit fontawesome",
+    properties: {
+      verison: "4.2.0"
+  })
   serve 'fa', 'fa.css'
 end
 
 get '/fa/fonts/fontawesome-webfont.:ext' do
+  Analytics.track(
+    user_id: request.ip,
+    event: "Hit fontwesome font",
+    properties: {
+      ext: params[:ext],
+      verison: params[:num]
+  })
   serve 'fa/fonts', "fontawesome-webfont.#{params[:ext]}"
 end
 
 get '/ps/ps.:ext' do
+  Analytics.track(
+    user_id: request.ip,
+    event: "Hit photoswipe #{params[:ext]}",
+    properties: {
+      ext: params[:ext]
+  })
   serve 'ps', "photoswipe.#{params[:ext]}"
 end
 
 get '/ps/ps-ui.js' do
+  Analytics.track(user_id: request.ip, event: "Hit Photoswipe ui")
   serve 'ps', 'photoswipe-ui-default.js'
 end
 
 get '/ps/skin/skin.:ext' do
+  Analytics.track(
+    user_id: request.ip,
+    event: "Hit photoswipe skin #{params[:ext]}",
+    properties: {
+      ext: params[:ext]
+  })
   serve 'ps/skin', "default-skin.#{params[:ext]}"
 end
 
 get '/ps/skin/preloader.gif' do
+  Analytics.track(user_id: request.ip, event: "Hit photoswipe preloader")
   serve 'ps/skin', 'preloader.gif'
 end
 
@@ -80,25 +123,42 @@ get '/bs/bs.:ext' do
   if params[:v] == nil
     params[:v] = "3.3.1"
   end
+  Analytics.track(
+    user_id: request.ip,
+    event: "Hit bootstrap #{params[:ext]} v#{params[:v]}",
+    properties: {
+      ext: params[:ext],
+      version: params[:v]
+  })
   serve "bs/#{params[:v]}", "bootstrap.#{params[:ext]}"
 end
 
 get '/bs/theme.css' do
+  Analytics.track(user_id: request.ip, event: "Hit bootstrap theme css")
   serve 'bs', 'bootstrap-theme.css'
 end
 
 get '/bs/fonts/glyphicons-halflings-regular.:ext' do
+  Analytics.track(user_id: request.ip, event: "Hit bootstrap font")
   serve 'bs/fonts', "glyphicons-halflings-regular.#{params[:ext]}"
 end
 
 get '/md/md.:ext' do
+  Analytics.track(
+    user_id: request.ip,
+    event: "Hit materialize #{params[:ext]}",
+    properties: {
+      ext: params[:ext]
+  })
   serve 'md', "materialize.#{params[:ext]}"
 end
 
 get '/md/font/mdi/Material-Design-Icons.:ext' do
+  Analytics.track(user_id: request.ip, event: "Hit materialize icons")
   serve 'md/font/mdi', "Material-Design-Icons.#{params[:ext]}"
 end
 
 get '/md/font/roboto/Roboto-:density.ttf' do
+  Analytics.track(user_id: request.ip, event: "Hit materialize font")
   serve '/md/font/roboto', "Roboto-#{params[:density]}.ttf"
 end
