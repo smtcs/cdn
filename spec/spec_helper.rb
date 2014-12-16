64a6fd32237ab7b8b767a6ded914e2580da81de8
ZucchiniZe <me@alexb.ninja>
2014-12-15 20:06:55 -0800

    get coverage to work

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,10 @@
+require 'codeclimate-test-reporter'
+CodeClimate::TestReporter.start
 ENV['RACK_ENV'] = 'test'
 require_relative '../app'
 require 'rspec'
-require 'codeclimate-test-reporter'
 require 'capybara/rspec'
 require 'capybara/poltergeist'
-
-CodeClimate::TestReporter.start
 
 Capybara.app = CDN::Main
 Capybara.javascript_driver = :poltergeist
