5cee6ef522dbb4b37b4574da1f496a8518cc5281
ZucchiniZe <me@alexb.ninja>
2015-01-06 10:36:43 -0800

    add flatdoc page

--- routes/misc.rb
+++ routes/misc.rb
@@ -12,6 +12,11 @@ module CDN
         erb :old
       end
 
+      app.get '/flat' do
+        Analytics.track(user_id: request.ip, event: 'Hit flatdoc')
+        erb :flat
+      end
+
       app.get '/test' do
         content_type :html
         IO.popen('bin/rspec -f h')
--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -2,12 +2,18 @@ require 'spec_helper'
 
 describe 'Home', :type => :feature do
   it 'new should 200 - /' do
-    visit "/"
+    visit '/'
     page.status_code.should == 200
   end
 
   it 'old should 200 - /old' do
     visit '/old'
+    page.status_code.should == 200
+    page.should have_content 'Summit CDN'
+  end
+
+  it 'flat should 200 - /flat' do
+    visit '/flat'
     page.status_code.should == 200
     page.should have_content 'Summit CDN'
   end
